c28daa0f3c0cec0594780a54266158587d43fe8c
Paul <dauriacpau@eisti.eu>
2020-08-11 11:35:36 +0200

    Add images and videos

--- User_interface.py
+++ User_interface.py
@@ -1,8 +1,7 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QGridLayout, QGroupBox, QLineEdit, QLabel,  QFrame,
                              QVBoxLayout, QWidget, QHBoxLayout, QPushButton, QFileDialog, QScrollArea, QFormLayout)
-from PyQt5 import QtGui
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QPixmap, QIcon
 
 
 # Function that return a QLabel with the user message color
@@ -33,7 +32,7 @@ def message_history():
         vertical_box.addWidget(user_input("Hello there, what's your name?"))
         vertical_box.addWidget(chatbot_input("i ' m felix , and i ' ve a brother . do you have any siblings ?"))
         vertical_box.addWidget(user_input("Yes I have 1 sister and 1 brother. How old is your brother?"))
-        vertical_box.addWidget(chatbot_input("he ' s 10 years old . what do you like to do for fun ? i study psychology ."))
+        vertical_box.addWidget(chatbot_input("he's 10 years old. what do you like to do for fun ? i study psychology."))
 
     # Add the messages to the groupbox
     group_box.setLayout(vertical_box)
@@ -42,8 +41,14 @@ def message_history():
 
 
 # Open the window in order to select the files
-def getfile(self):
-    image = QFileDialog.getOpenFileName(self, 'Open file', 'c:\\', "Image/Videos (*.png *.jpg *.gif *.mp4 *.wav)")
+def getfile(self, box):
+    (image, _) = QFileDialog.getOpenFileName(self, 'Open file', 'c:\\', "Image/Videos (*.png *.jpg *.gif *.mp4 *.wav)")
+    print(image)
+    pixmap = QPixmap(image)
+    pixmap = pixmap.scaledToWidth(600)
+    image_input = QLabel(self)
+    image_input.setPixmap(pixmap)
+    box.addWidget(image_input)
 
 
 # When the user send a message
@@ -75,7 +80,7 @@ def messages(messages_box):
     # Add a button in order to input photos and videos
     import_file = QPushButton()
     import_file.setIcon(QIcon("Images/photo.png"))
-    # import_file.clicked.connect(getfile(import_file))
+    import_file.clicked.connect(lambda: getfile(import_file, messages_box))
     horizontal_box.addWidget(import_file)
 
     group_box.setLayout(horizontal_box)
@@ -107,7 +112,6 @@ class Window(QWidget):
         grid = QGridLayout(self.scrollAreaWidgetContents)
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         self.layout.addWidget(self.scrollArea)
-
         # Add the message history
         messages_history, vertical_box = message_history()
         grid.addWidget(messages_history)
@@ -124,7 +128,6 @@ class Window(QWidget):
 
 
 # TODO: add persona
-# TODO: add video/images
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     clock = Window()
