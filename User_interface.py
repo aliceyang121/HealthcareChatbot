2bee05a417497bc036e4909d55bd891c3093b8b4
Paul <dauriacpau@eisti.eu>
2020-08-12 17:02:36 +0200

    Add menu and fix scrollbar

--- User_interface.py
+++ User_interface.py
@@ -1,8 +1,8 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QGridLayout, QGroupBox, QLineEdit, QLabel,  QFrame,
-                             QVBoxLayout, QWidget, QHBoxLayout, QPushButton, QFileDialog, QScrollArea, QFormLayout)
-from PyQt5 import QtGui
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QApplication, QGridLayout, QGroupBox, QLineEdit, QLabel, QFrame,
+                             QVBoxLayout, QWidget, QHBoxLayout, QPushButton, QFileDialog, QScrollArea, QFormLayout,
+                             QMainWindow, QMessageBox, QAction)
+from PyQt5.QtGui import QIcon, QPixmap
 from emotion_recognition import detect_emotion
 
 
@@ -27,24 +27,35 @@ def chatbot_input(text):
 
 
 def message_history():
-    group_box = QGroupBox("Messages")
+    widget = QGroupBox("Message")
+    scroll = QScrollArea()
+    scroll.setWidget(widget)
+    scroll.setMinimumHeight(600)
+    scroll.setWidgetResizable(True)
     vertical_box = QVBoxLayout()
+
     # Add some predefined messages
-    for i in range(3):
+    for i in range(12):
         vertical_box.addWidget(user_input("Hello there, what's your name?", "joy"))
         vertical_box.addWidget(chatbot_input("i ' m felix , and i ' ve a brother . do you have any siblings ?"))
         vertical_box.addWidget(user_input("Yes I have 1 sister and 1 brother. How old is your brother?", "joy"))
-        vertical_box.addWidget(chatbot_input("he ' s 10 years old . what do you like to do for fun ? i study psychology ."))
+        vertical_box.addWidget(chatbot_input("he's 10 years old. what do you like to do for fun? i study psychology ."))
 
-    # Add the messages to the groupbox
-    group_box.setLayout(vertical_box)
-    # Return the groupbox and the verticalbox in order to update it
-    return group_box, vertical_box
+    # Add the messages to the box
+    widget.setLayout(vertical_box)
+    # Return the scrollbar and the verticalbox in order to update it
+    return scroll, vertical_box
 
 
 # Open the window in order to select the files
-def getfile(self):
-    image = QFileDialog.getOpenFileName(self, 'Open file', 'c:\\', "Image/Videos (*.png *.jpg *.gif *.mp4 *.wav)")
+def getfile(self, box):
+    (image, _) = QFileDialog.getOpenFileName(self, 'Open file', 'c:\\', "Image/Videos (*.png *.jpg *.gif *.mp4 *.wav)")
+    print(image)
+    pixmap = QPixmap(image)
+    pixmap = pixmap.scaledToWidth(600)
+    image_input = QLabel(self)
+    image_input.setPixmap(pixmap)
+    box.addWidget(image_input)
 
 
 # When the user send a message
@@ -78,7 +89,7 @@ def messages(messages_box):
     # Add a button in order to input photos and videos
     import_file = QPushButton()
     import_file.setIcon(QIcon("Images/photo.png"))
-    # import_file.clicked.connect(getfile(import_file))
+    import_file.clicked.connect(lambda: getfile(import_file, messages_box))
     horizontal_box.addWidget(import_file)
 
     group_box.setLayout(horizontal_box)
@@ -97,44 +108,66 @@ def new_message_on_bottom():
     return frame
 
 
-class Window(QWidget):
-    def __init__(self, parent=None):
-        super(Window, self).__init__(parent)
+def change_persona():
+    print('Hello there')
 
-        self.add_scrollbar()
 
-        grid = QGridLayout(self.scrollAreaWidgetContents)
+class UserInterface(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
 
+        self.layout = QHBoxLayout(self)
+        self.title = "Healthcare Chatbot"
+        self.setWindowTitle("Healthcare Chatbot")
+        self.resize(720, 720)
+        self.add_scrollbar_widgets()
+        self.set_menu()
+
+    def reset_chatbot(self):
+        alert = QMessageBox.about(self, "Warning", "Are you sure you want to reset the "
+                                                   "chatbot?\n All data will be loss")
+
+        #alert.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        # alert.buttonClicked.connect(msgButtonClick)
+        # returnValue = alert.exec()
+        # if returnValue == QMessageBox.Ok:
+        #     print('OK clicked')
+
+        # TODO: Reset the persona
+        # TODO: Clear history
+        # TODO: Print the Qmessage with the ok and cancel buttons
+        # blender_agent = create_agent_from_model_file("zoo:blender/blender_90M/model")
+
+    def set_menu(self):
+        persona = QAction("Change persona", self)
+        persona.triggered.connect(lambda: change_persona())
+
+        reset = QAction("Reset Chatbot", self)
+
+        reset.triggered.connect(lambda: self.reset_chatbot())
+        menu = self.menuBar()
+        menu.addAction(persona)
+        menu.addAction(reset)
+
+    def add_scrollbar_widgets(self):
+        # Initialise grid and add the QGridLayout to the QWidget that is added to the QScrollArea
+        grid = QGridLayout(self)
         # Add the message history
         messages_history, vertical_box = message_history()
         grid.addWidget(messages_history)
-
         # Separation between the new messages input and the history
         grid.addWidget(new_message_on_bottom())
 
         # Add the input line for new messages
         grid.addWidget(messages(vertical_box))
-        self.setLayout(grid)
-
-        self.setWindowTitle("Healthcare Chatbot")
-        self.resize(720, 720)
-
-
-    def add_scrollbar(self):
-        # Adding scrollbar
-        self.layout = QHBoxLayout(self)
-        self.scrollArea = QScrollArea(self)
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
-        # Add the QGridLayout to the QWidget that is added to the QScrollArea
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-        self.layout.addWidget(self.scrollArea)
+        self.central_widget.setLayout(grid)
 
 
 # TODO: add persona
-# TODO: add video/images
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    clock = Window()
-    clock.show()
+    user_interface = UserInterface()
+    user_interface.show()
     sys.exit(app.exec_())
